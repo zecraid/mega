1e6305e266fee3200b8cd44da3670f89ba1bf6b4
zecraid <zecraid@gmail.com>
2023-09-05 18:30:00 +0800

    rebuild +BlockQueue test

--- src/log/test.cpp
+++ src/log/test.cpp
@@ -2,8 +2,8 @@
 
 int main(){
     Log::Instance()->init(0, "./log", ".log", 0);
-    LOG_DEBUG("test debug\n");
-    LOG_INFO("test info\n");
-    LOG_WARN("test warn\n");
-    LOG_ERROR("test error\n");
+    LOG_DEBUG("test debug");
+    LOG_INFO("test info");
+    LOG_WARN("test warn");
+    LOG_ERROR("test error");
 }
