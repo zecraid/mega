a306f70a8bc84b01e4970ff173c198107c62944d
zecraid <zecraid@gmail.com>
2023-09-10 20:33:51 +0800

    rebuild +BlockQueue test

--- src/server/WebServer.cpp
+++ src/server/WebServer.cpp
@@ -7,13 +7,13 @@ WebServer::WebServer(const char *ip, uint16_t port) {
     std::function<void(int)> cb = std::bind(&WebServer::newConnection, this, std::placeholders::_1);
     acceptor_->setNewConnectionCallback(cb);
 
-//    unsigned int size = std::thread::hardware_concurrency();
-//    thread_pool_ = std::make_unique<ThreadPool>(size);
-//
-//    for(size_t i = 0; i < size; i++){
-//        std::unique_ptr<EventLoop> sub = std::make_unique<EventLoop>(true);
-//        sub_reactors_.push_back(std::move(sub));
-//    }
+    unsigned int size = std::thread::hardware_concurrency();
+    thread_pool_ = std::make_unique<ThreadPool>(size);
+
+    for(size_t i = 0; i < size; i++){
+        std::unique_ptr<EventLoop> sub = std::make_unique<EventLoop>(true);
+        sub_reactors_.push_back(std::move(sub));
+    }
 }
 
 void WebServer::init(int logLevel, int logQueSize, const char *sqlLocal, uint16_t sqlPort, const char *sqlUser,
@@ -36,10 +36,10 @@ void WebServer::init(int logLevel, int logQueSize, const char *sqlLocal, uint16_
 }
 
 void WebServer::start() {
-//    for(size_t i = 0; i < sub_reactors_.size(); i++){
-//        std::function<void()> sub_loop = std::bind(&EventLoop::loop, sub_reactors_[i].get());
-//        thread_pool_->add(std::move(sub_loop));
-//    }
+    for(size_t i = 0; i < sub_reactors_.size(); i++){
+        std::function<void()> sub_loop = std::bind(&EventLoop::loop, sub_reactors_[i].get());
+        thread_pool_->add(std::move(sub_loop));
+    }
     LOG_INFO("Web Server Start...SUCCESS");
     main_reactor_->loop();
 }
@@ -47,10 +47,10 @@ void WebServer::start() {
 void WebServer::newConnection(int fd) {
     assert(fd != -1);
     LOG_INFO("new client Comein");
-//    uint16_t random = fd % sub_reactors_.size();
-//    std::unique_ptr<HttpConnection> conn = std::make_unique<HttpConnection>(fd, sub_reactors_[random].get());
-//    std::function<void(int)> cb = std::bind(&WebServer::closeConnection, this, std::placeholders::_1);
-//    conn->setCloseConnectionCallback(cb);
+    uint16_t random = fd % sub_reactors_.size();
+    std::unique_ptr<HttpConnection> conn = std::make_unique<HttpConnection>(fd, sub_reactors_[random].get());
+    std::function<void(int)> cb = std::bind(&WebServer::closeConnection, this, std::placeholders::_1);
+    conn->setCloseConnectionCallback(cb);
 
 //    connections_[fd] = std::move(conn); // err
     LOG_INFO("new client Add fd = %d",fd);
