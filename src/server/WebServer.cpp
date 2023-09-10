5de51e7da3bb152d9b20540d4ba365a3c27a9869
zecraid <zecraid@gmail.com>
2023-09-10 18:45:20 +0800

    rebuild +BlockQueue test

--- src/server/WebServer.cpp
+++ src/server/WebServer.cpp
@@ -52,7 +52,7 @@ void WebServer::newConnection(int fd) {
     std::function<void(int)> cb = std::bind(&WebServer::closeConnection, this, std::placeholders::_1);
     conn->setCloseConnectionCallback(cb);
 //    connections_[fd] = std::move(conn);
-    LOG_INFO("new client Add fd = %d, address:",conn->getFd(), conn->getAddr());
+    LOG_INFO("new client Add fd = %d, address:%s",conn->getFd(), conn->getAddr());
 //    return ST_SUCCESS;
 }
 
