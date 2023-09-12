be8cc7577f5f00477125f56238ec7cdd94bcaa2d
zecraid <zecraid@gmail.com>
2023-09-12 10:28:07 +0800

    rebuild +BlockQueue test

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include "src/server/WebServer.h"
 
 int main(){
-    std::unique_ptr<WebServer> server = std::make_unique<WebServer>("0.0.0.0",8888);
-//    WebServer * server = new WebServer("0.0.0.0",8888);
+//    std::unique_ptr<WebServer> server = std::make_unique<WebServer>("0.0.0.0",8888);
+    WebServer * server = new WebServer("0.0.0.0",8888);
     server->init(0, 0, "localhost", 3306, "root", "123456", "webserver", 12, "./resources");
     server->start();
 }
